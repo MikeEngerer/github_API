64aa480d1a73f5c22e491daa04d2d61260fcd648
MikeEngerer <michael.engerer@hotmail.com>
2018-12-03 21:44:32 +0000

    added functionality to getRepoContributors - now console.logs each user's avatar_url

--- github_API/download_avatars.js
+++ github_API/download_avatars.js
@@ -4,18 +4,25 @@ var GITHUB_TOKEN = require('./secrets.js')
 function getRepoContributors(repoOwner, repoName, cb) {
  
   var options = {
-  	url: "https://api.github.com/repos/${repoOwner}/${repoName}/contributors",
+	url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
   	headers: {
   		'User-Agent': 'request',
   		Authorization: GITHUB_TOKEN
   	}
   }
+
   request(options, function(err, res, body) {
   	cb(err, body)
   })
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+var cb = function(err, body) {
+	if (err) throw err;
+	var parsedJSON = JSON.parse(body);
+	parsedJSON.forEach(function(obj) {
+		console.log(obj.avatar_url);
+	});
+};
+
+getRepoContributors("jquery", "jquery", cb)
+
