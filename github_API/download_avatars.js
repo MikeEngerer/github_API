aab86c583cb2743629b13541cb231a4c93e5a279
MikeEngerer <michael.engerer@hotmail.com>
2018-12-03 21:32:22 +0000

    added token authorization via options.header

--- github_API/download_avatars.js
+++ github_API/download_avatars.js
@@ -1,7 +1,18 @@
 var request = require('request');
+var GITHUB_TOKEN = require('./secrets.js')
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+ 
+  var options = {
+  	url: "https://api.github.com/repos/${repoOwner}/${repoName}/contributors",
+  	headers: {
+  		'User-Agent': 'request',
+  		Authorization: GITHUB_TOKEN
+  	}
+  }
+  request(options, function(err, res, body) {
+  	cb(err, body)
+  })
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
